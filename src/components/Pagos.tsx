f2a1c03eb662435426e5b6568a5192706b068ccf
Xseb4s <sebas25qr@gmail.com>
2024-05-20 13:49:49 -0500

    feat: added the pays component

---
+++ src/components/Pagos.tsx
@@ -0,0 +1,14 @@
+import Form from "./Form"
+
+const Pagos = () => {
+  return (
+    <>
+        <div>
+            <h1 className="text-primary">Pagos</h1>
+            <Form/>
+        </div>
+    </>
+  )
+}
+
+export default Pagos
