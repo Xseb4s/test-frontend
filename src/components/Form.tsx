64b2fb57343394361798263c3631dceb9ef6383d
Xseb4s <sebas25qr@gmail.com>
2024-05-23 23:43:57 -0500

    feat: updated to edit one by one the payments

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,92 +1,122 @@
-import React, { useState } from 'react';
-import { useForm, Controller, SubmitHandler } from 'react-hook-form';
-import { z, ZodType } from 'zod';
+import { useState } from 'react';
+import { useStore } from '../store/useStore';
+import { FaRegCircle } from 'react-icons/fa';
+import { CiCircleMinus, CiCirclePlus } from 'react-icons/ci';
+import { Controller, useForm } from 'react-hook-form';
+import { MdOutlineDateRange } from 'react-icons/md';
+import ReactDatePicker from 'react-datepicker';
 import { zodResolver } from '@hookform/resolvers/zod';
-import DatePicker from 'react-datepicker';
-import { FaRegCircle } from "react-icons/fa";
-import 'react-datepicker/dist/react-datepicker.css';
+import { EditSchema, editSchema } from '../schemas/editSchema';
+import useEditStore from '../store/useEditStore';
 
-const schema: ZodType<{ title: string; status: 'pagado' | 'pendiente'; date: Date | null; pay: number }> = z.object({
-  title: z.string().min(1, 'El título es requerido'),
-  status: z.enum(['pagado', 'pendiente']).refine(val => val === 'pagado' || val === 'pendiente', {
-    message: 'El estado es requerido',
-  }),
-  date: z.date().nullable().refine(date => date instanceof Date, {
-    message: 'Fecha inválida',
-  }),
-  pay: z.number().min(0, 'El monto debe ser mayor o igual a cero'),
-});
+// este archivo lo guardaría en credit/[idCredit]/index.tsx
+// el componente recibiría el id y usaría la api de leer los pagos por el id del crédito 
+/*
+const getDataPaymentsByCreditId = async () => { 
+  const {error} = await ReadPaymentsByCreditId(idCredit);  // esto vendría de services/pagos.routes
+}
+iteraría la cantidad de pagos sobre la api de update de UpdatePaymentsById(idPayment)
+*/
 
-type FormData = z.infer<typeof schema>;
-
-const Form: React.FC = () => {
-  const { control, handleSubmit, register, formState: { errors } } = useForm<FormData>({
-    resolver: zodResolver(schema),
+const Form = () => {
+  const credits = useStore((state) => state.credits);
+  const editPayment = useEditStore((state) => state.editPayment);
+  const { editingCreditId } = useEditStore();
+  const credit = credits.find((c) => c.id === editingCreditId);
+  const [percentage, setPercentage] = useState<number>(0);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EditSchema>({
+    resolver: zodResolver(editSchema),
   });
 
-  const [formattedPay, setFormattedPay] = useState<string>('');
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    //devuelve objeto
-    //pasar a aray de objetos (json)
-    console.log(data);
+  const restar = () => {
+    setPercentage((prevPercentage) => prevPercentage - 1);
   };
 
-  const handlePayChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseFloat(e.target.value.replace(',', '.'));
-    if (!isNaN(value)) {
-      const formattedValue = value.toFixed(1).replace('.', ',');
-      setFormattedPay(formattedValue);
-    }
-  }; 
+  const sumar = () => {
+    setPercentage((prevPercentage) => prevPercentage + 1);
+  };
 
-  const minDate = new Date();
+  const onSubmit = (data: EditSchema) => {
+    const editPaymentSchema = {
+      ...data,
+      percentage: percentage,
+    };
+    editPayment(editingCreditId, editPaymentSchema);
+    
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='text-primary flex flex-col items-center gap-2 w-fit'>
-      <div>
-        <FaRegCircle className='w-8 h-8  text-red-500'/>
-      </div>
-      <div>
-        <input id="title" {...register('title')} placeholder='Nuevo' className='text-black'/>
-        {errors.title && <p className='text-red-400'>{errors.title.message}</p>}
-      </div>
+    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-row w-64 items-center gap-2 mr-10'>
+      {credit?.payments.map((payment) => (
+        <div key={payment.id} className='flex flex-col items-center'>
+          <FaRegCircle className='w-8 h-8 text-red-500' />
+          <input
+            type='text'
+            placeholder='Nuevo pago'
+            className='outline outline-1 w-full'
+            defaultValue={payment.title}
+            {...control.register('title')}
+          />
 
-      <div>
-        <label htmlFor="status" className='mr-1'>Estado</label>
-        <select id="status" {...register('status')} className='text-black p-1'>
-          <option value="pendiente">Pendiente</option>
-          <option value="pagado">Pagado</option>
-        </select>
-        {errors.status && <p className='text-red-400'>{errors.status.message}</p>}
-      </div>
-
-      <div>
-        <input type="text" id="pay" value={formattedPay} onChange={handlePayChange} placeholder='$' className='text-black focus:outline-none'/>
-        <span>USD</span>
-      </div>
-
-      <div className='flex flex-col'>
-        <label htmlFor="date" className='mr-1'>Vence</label>
-        <Controller
-          name="date"
-          control={control}
-          defaultValue={null}
-          render={({ field }) => (
-            <DatePicker
-              selected={field.value}
-              placeholderText='--/--/----'
-              onChange={(date) => field.onChange(date as Date)}
-              dateFormat="dd/MM/yyyy"
-              minDate={minDate}
-              className='text-black p-1 custom-datepicker'
-              locale='es'
+          <div className='flex gap-2 border border-black w-full'>
+            <input
+              type='number'
+              placeholder='Monto'
+              className='outline outline-1 w-full'
+              defaultValue={payment.amount}
+              {...control.register('amount', { valueAsNumber: true })}
             />
-          )}
-        />
-        {errors.date && <p className='text-red-400'>{errors.date?.message}</p>}
-      </div>
+            <span className='px-1'>USD</span>
+          </div>
+          
 
-      <button type="submit">Guardar</button>
+          <div className='flex gap-2 w-full'>
+            <CiCircleMinus className='w-7 h-7 text-red-400 cursor-pointer' onClick={restar} />
+
+            <div className='flex gap-2 border border-black'>
+              <input
+                type='number'
+                className='outline-none w-full'
+                defaultValue={percentage}
+                readOnly
+              />
+              <span className='px-1'>%</span>
+            </div>
+            <CiCirclePlus className='w-7 h-7 text-red-400 cursor-pointer' onClick={sumar} />
+          </div>
+          <div className='flex flex-col'>
+            <label htmlFor='date' className='mr-1 text-gray-400'>
+              Vence
+            </label>
+            <Controller
+              name='date'
+              control={control}
+              render={({ field }) => (
+                <div className='flex items-center gap-2'>
+                  <MdOutlineDateRange className='w-5 h-5 text-red-400' />
+                  <ReactDatePicker
+                    selected={field.value}
+                    onChange={(date) => field.onChange(date)}
+                    dateFormat='dd/MM/yyyy'
+                    minDate={new Date()}
+                    className='text-red-400 p-1 custom-datepicker cursor-pointer'
+                  />
+                </div>
+              )}
+            />
+
+          </div>
+        </div>
+      ))}
+
+      <button type='submit' className='text-red-400 hover:bg-red-400 hover:text-white rounded-lg px-1'>
+        Guardar
+      </button>
     </form>
   );
 };
